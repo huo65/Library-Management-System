de1e437e3944b9e61503dfbf4c8f97ab35862175
huo65 <1599272181@qq.com>
2024-04-21 16:57:42 +0800

    fix:登录注册界面调整

--- vue/src/views/Forget.vue
+++ vue/src/views/Forget.vue
@@ -1,27 +1,27 @@
 <template>
   <div  class="login-container"  >
     <el-form ref="form" :model="form"    :rules="rules" class="login-page">
-      <h2 class="title" style="margin-bottom: 20px">密码重置</h2>
+      <h2 class="title" style="margin-bottom: 20px">Reset Password</h2>
       <el-form-item prop="username" >
-        <el-input v-model="form.username" placeholder="请输入您的用户名" clearable >
+        <el-input v-model="form.username" placeholder="username" clearable >
         </el-input>
       </el-form-item>
       <el-form-item prop="password">
-        <el-input v-model="form.password"  placeholder="请输入新密码" clearable show-password>
+        <el-input v-model="form.password"  placeholder="new password" clearable show-password>
         </el-input>
       </el-form-item>
       <el-form-item prop="confirm">
-        <el-input v-model="form.confirm" placeholder="请再次确认密码" clearable show-password>
+        <el-input v-model="form.confirm" placeholder="confirm password" clearable show-password>
         </el-input>
       </el-form-item>
       <el-form-item>
-          <el-input  v-model="form.code" style="width: 45%;" placeholder="请输入验证码"></el-input>
+          <el-input  v-model="form.code" style="width: 45%;" placeholder="verification code"></el-input>
           <el-button type="success" plain  style="margin-left: 10px" @click="getcode" :disabled="isDisabled">{{buttonName}}</el-button>
       </el-form-item>
       <el-form-item >
-        <el-button type="primary" style=" width: 100%;font-size: 18px" @click="register">密码重置</el-button>
+        <el-button type="primary" style=" width: 100%;font-size: 18px" @click="register">reset password</el-button>
       </el-form-item>
-      <el-form-item><el-button type="text" style="font-size: 16px"  @click="$router.push('/login')">前往登录>> </el-button></el-form-item>
+      <el-form-item><el-button type="text" style="font-size: 16px"  @click="$router.push('/login')">Log in>> </el-button></el-form-item>
     </el-form>
   </div>
 
@@ -34,7 +34,7 @@ export default {
   name: "Forget",
   data(){
     return{
-      buttonName: "获取短信验证码",
+      buttonName: "Get verification code",
       isDisabled: false,
       time: 60,
       form:{},
@@ -42,27 +42,27 @@ export default {
         username: [
           {
             required: true,
-            message: '请输入用户名',
+            message: 'enter username',
             trigger: 'blur',
           },
           {
             min: 2,
             max: 13,
-            message: '长度要求为2到13位',
+            message: 'Length requirement: 2~13',
             trigger: 'blur',
           },
         ],
         password: [
           {
             required: true,
-            message: '请输入密码',
+            message: 'enter password',
             trigger: 'blur',
           }
         ],
         confirm:[
           {
             required:true,
-            message:"请确认密码",
+            message:"confirm password",
             trigger:"blur"
           }
         ],
@@ -73,16 +73,16 @@ export default {
   methods:{
     getcode(){
       if (!this.form.username) {
-        ElMessage.error("请填写用户名")
+        ElMessage.error("username")
         return
       }
       if (!this.form.password) {
-        ElMessage.error("请填写新密码")
+        ElMessage.error("new password")
         return
       }
       if(this.form.password != this.form.confirm)
       {
-        ElMessage.error("两次密码输入不一致")
+        ElMessage.error("The two password entries do not match")
         return
       }
       request.get("forget/getcode",{
@@ -91,14 +91,14 @@ export default {
         }
       }).then(res=>{
         if (res.code == 0) {
-          ElMessage.success("验证码发送成功")
+          ElMessage.success("Sent successful")
           let me = this;
           me.isDisabled = true;
           let interval = window.setInterval(function() {
-            me.buttonName = '（' + me.time + '秒）后重新发送';
+            me.buttonName = '（' + me.time + 's）sent again';
             --me.time;
             if(me.time < 0) {
-              me.buttonName = "重新发送";
+              me.buttonName = "sent again";
               me.time = 60;
               me.isDisabled = false;
               window.clearInterval(interval);
@@ -113,22 +113,22 @@ export default {
       this.$refs['form'].validate((valid) => {
         if (valid) {
           if (!this.form.code) {
-            ElMessage.error("请填写验证码")
+            ElMessage.error("verification")
             return
           }
           if (!this.form.password) {
-            ElMessage.error("请填写新密码")
+            ElMessage.error("new password")
             return
           }
           if(this.form.password != this.form.confirm)
           {
-            ElMessage.error("两次密码输入不一致")
+            ElMessage.error("The two password entries do not match")
             return
           }
           request.post("forget/register",this.form).then(res=>{
             if(res.code == 0)
             {
-              ElMessage.success("密码修改成功")
+              ElMessage.success("Password change successful")
               this.$router.push("/login")
             }
             else {ElMessage.error(res.msg)}
@@ -148,16 +148,26 @@ export default {
   position: fixed;
   width: 100%;
   height: 100%;
-  background: url('../img/bg2.svg');
-  background-size: contain;
+  background: url('../img/bg4.jpg');
+  background-size: cover;
 }
 .login-page {
-  border-radius: 5px;
-  margin: 180px auto;
+  position:relative;
+  border-radius: 15px;
   width: 350px;
   padding: 35px 35px 15px;
   background: #fff;
   border: 1px solid #eaeaea;
-  box-shadow: 0 0 25px #cac6c6;
+  box-shadow: 0 5px 25px rgba(0,0,0,0.25);
+  left: 50%;
+  top: 50%;
+  transform: translate(-50%, -50%);
 }
+.title {
+    font-size: 33px;
+    font-weight: bold;
+    text-align: center;
+    line-height: 100px;
+}
+
 </style>
--- vue/src/views/Register.vue
+++ vue/src/views/Register.vue
@@ -1,34 +1,34 @@
 <template>
 <div  class="login-container"  >
     <el-form ref="form" :model="form"    :rules="rules" class="login-page">
-      <h2 class="title" style="margin-bottom: 20px">用户注册</h2>
+      <h2 class="title" style="margin-bottom: 20px">Sign up</h2>
       <el-form-item prop="username" >
-        <el-input v-model="form.username" placeholder="请输入用户名" clearable >
+        <el-input v-model="form.username" placeholder="username" clearable >
           <template #prefix>
             <el-icon class="el-input__icon"><User/></el-icon>
           </template>
         </el-input>
       </el-form-item>
       <el-form-item prop="password">
-        <el-input v-model="form.password"  placeholder="请输入密码" clearable show-password>
+        <el-input v-model="form.password"  placeholder="password" clearable show-password>
           <template #prefix>
             <el-icon class="el-input__icon"><Lock /></el-icon>
           </template>
         </el-input>
       </el-form-item>
       <el-form-item prop="confirm">
-        <el-input v-model="form.confirm" placeholder="请再次确认密码" clearable show-password>
+        <el-input v-model="form.confirm" placeholder="confirm password" clearable show-password>
           <template #prefix>
             <el-icon class="el-input__icon"><Lock /></el-icon>
           </template>
         </el-input>
       </el-form-item>
       <el-form-item prop="role">
-        <el-radio v-model="form.role" label="3">读者</el-radio>
+        <el-radio v-model="form.role" label="3">reader</el-radio>
 <!--        <el-radio v-model="form.role" label="1">管理员</el-radio>-->
       </el-form-item>
       <el-form-item prop="authorize" v-if="form.role==1">
-        <el-input v-model="form.authorize" placeholder="请输入管理员注册码" clearable show-password>
+        <el-input v-model="form.authorize" placeholder="enter the administrator registration code" clearable show-password>
           <template #prefix>
             <el-icon class="el-input__icon"><Lock /></el-icon>
           </template>
@@ -36,14 +36,14 @@
       </el-form-item>
       <el-form-item>
         <div style="display: flex">
-          <el-input  v-model="form.validCode" style="width: 45%;" placeholder="请输入验证码"></el-input>
+          <el-input  v-model="form.validCode" style="width: 52%;" placeholder="verification code"></el-input>
           <ValidCode @input="createValidCode" style="width: 50%"/>
         </div>
       </el-form-item>
       <el-form-item >
-        <el-button type="primary" style=" width: 100%;font-size: 20px"  @click="register">注 册</el-button>
+        <el-button type="primary" style=" width: 100%;font-size: 20px"  @click="register">sign up</el-button>
       </el-form-item>
-      <el-form-item><el-button type="text" style="font-size: 16px" @click="$router.push('/login')">前往登录>> </el-button></el-form-item>
+      <el-form-item><el-button type="text" style="font-size: 16px" @click="$router.push('/login')">Log in>> </el-button></el-form-item>
     </el-form>
 </div>
 
@@ -66,34 +66,34 @@ export default {
         username: [
           {
             required: true,
-            message: '请输入用户名',
+            message: 'enter username',
             trigger: 'blur',
           },
           {
             min: 2,
             max: 13,
-            message: '长度要求为2到13位',
+            message: 'Length requirement: 2~13',
             trigger: 'blur',
           },
         ],
         password: [
           {
             required: true,
-            message: '请输入密码',
+            message: 'enter password',
             trigger: 'blur',
           }
         ],
       confirm:[
         {
           required:true,
-          message:"请确认密码",
+          message:"confirm password",
           trigger:"blur"
         }
       ],
         authorize:[
           {
             required:true,
-            message:"请输入注册码",
+            message:"Registration code",
             trigger:"blur"
           }
         ],
@@ -109,27 +109,27 @@ export default {
       this.$refs['form'].validate((valid) => {
         if (valid) {
           if (!this.form.validCode) {
-            ElMessage.error("请填写验证码")
+            ElMessage.error("Please enter the verification code")
             return
           }
           if(this.form.validCode.toLowerCase() !== this.validCode.toLowerCase()) {
-            ElMessage.error("验证码错误")
+            ElMessage.error("verification code error")
             return
           }
           if(this.form.password != this.form.confirm)
           {
-            ElMessage.error("两次密码输入不一致")
+            ElMessage.error("The two password entries do not match")
             return
           }
           if(this.form.role == 1 && this.form.authorize != "2236")
           {
-            ElMessage.error("请输入正确的注册码")
+            ElMessage.error("Please enter the correct registration code")
             return
           }
           request.post("user/register",this.form).then(res=>{
             if(res.code == 0)
             {
-              ElMessage.success("注册成功")
+              ElMessage.success("Signup successful")
               this.$router.push("/login")
             }
             else {ElMessage.error(res.msg)}
@@ -149,16 +149,28 @@ export default {
   position: fixed;
   width: 100%;
   height: 100%;
-  background: url('../img/bg2.svg');
-  background-size: contain;
+  background: url('../img/bg5.jpg');
+  background-size: cover;
 }
 .login-page {
-  border-radius: 5px;
-  margin: 180px auto;
+  position: relative;
+  border-radius: 15px;
   width: 350px;
-  padding: 35px 35px 15px;
+  padding: 0 50px;
   background: #fff;
   border: 1px solid #eaeaea;
-  box-shadow: 0 0 25px #cac6c6;
+  box-shadow: 0 5px 25px rgba(0,0,0,0.25);
+  left: 50%;
+  top: 50%;
+  transform: translate(-50%, -50%);
+}
+.title {
+    font-size: 35px;
+    font-weight: bold;
+    text-align: center;
+    line-height: 100px;
+}
+::v-deep .el-input__inner{
+  height: 50px;
 }
 </style>
