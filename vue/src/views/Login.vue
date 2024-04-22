a6fb55d8bc50ce43b3f79ae996fca4ef2808411e
huo65 <1599272181@qq.com>
2024-04-22 16:43:07 +0800

    fix:登录页优化

--- vue/src/views/Login.vue
+++ vue/src/views/Login.vue
@@ -1,44 +1,37 @@
 <template>
-  <div class="login-container">
-    <el-form ref="form" :model="form" :rules="rules" class="login-page">
-      <h2 class="title" style="margin-bottom: 20px;text-align: center">Library Management System Login</h2>
-      <el-form-item prop="username">
-        <el-input v-model="form.username" placeholder="Username" clearable>
+<div  class="login-container"  >
+  
+    <el-form ref="form" :model="form"   :rules="rules" class="login-page">
+      <h2 class="title" style="margin-bottom: 20px;text-align: center">Login</h2>
+      <el-form-item prop="username" >
+        <el-input v-model="form.username"  placeholder="Username" clearable>
           <template #prefix>
-            <el-icon class="el-input__icon">
-              <User/>
-            </el-icon>
+            <el-icon class="el-input__icon"><User /></el-icon>
           </template>
         </el-input>
       </el-form-item>
       <el-form-item prop="password">
         <el-input v-model="form.password" placeholder="Password" clearable show-password>
           <template #prefix>
-            <el-icon class="el-input__icon">
-              <Lock/>
-            </el-icon>
+            <el-icon class="el-input__icon"><Lock /></el-icon>
           </template>
         </el-input>
       </el-form-item>
       <el-form-item>
         <div style="display: flex">
-          <el-input v-model="form.validCode" style="width: 45%;" placeholder="Verification code"></el-input>
+          <el-input  v-model="form.validCode" style="width: 45%;" placeholder="Captcha"></el-input>
           <ValidCode @input="createValidCode" style="width: 50%"/>
         </div>
       </el-form-item>
       <el-form-item>
-        <el-button type="primary" style=" width: 100%;height: 130%;font-size: 16px" @click="login">Login</el-button>
+        <el-button type="primary"  style=" width: 100%;height: 130%;font-size: 16px" @click="login">Log in</el-button>
       </el-form-item>
       <el-form-item>
-        <el-button type="text" style="font-size: 16px;width: 20%; color: #ee7463" @click="$router.push('/forget')">
-          Forgot password?
-        </el-button>
-        <el-button type="text" style="font-size: 16px;margin-left: 120px" @click="$router.push('/register')">New to LMS?
-          Sign up
-        </el-button>
+        <el-button type="text" style="font-size: 16px;width: 20%; color: #ee7463" @click="$router.push('/forget')">Forgot password?</el-button>
+        <el-button type="text" style="font-size: 16px;margin-left: 139px" @click="$router.push('/register')">Sign up</el-button>
       </el-form-item>
     </el-form>
-  </div>
+</div>
 
 </template>
 
@@ -49,7 +42,7 @@ import ValidCode from "../components/Validate";
 
 export default {
   name: "Login",
-  components: {
+  components:{
     ValidCode
   },
   data() {
@@ -60,14 +53,14 @@ export default {
         username: [
           {
             required: true,
-            message: 'Please enter your username',
+            message: 'username',
             trigger: 'blur',
           }
         ],
         password: [
           {
             required: true,
-            message: 'Please enter your password',
+            message: 'password',
             trigger: 'blur',
           }
         ]
@@ -77,25 +70,25 @@ export default {
     }
   },
   methods: {
-    createValidCode(data) {
+    createValidCode(data){
       this.validCode = data
     },
-    login() {
+    login(){
       this.$refs['form'].validate((valid) => {
         if (valid) {
           if (!this.form.validCode) {
-            ElMessage.error("Please enter the verification code")
+            ElMessage.error("enter the verification code")
             return
           }
-          if (this.form.validCode.toLowerCase() !== this.validCode.toLowerCase()) {
-            ElMessage.error("Incorrect verification code")
+          if(this.form.validCode.toLowerCase() !== this.validCode.toLowerCase()) {
+            ElMessage.error("verification code error")
             return
           }
 
           request.post("user/login", this.form).then(res => {
             if (res.code == 0) {
-              ElMessage.success("login success")
-              sessionStorage.setItem("user", JSON.stringify(res.data))//缓存用户信息
+              ElMessage.success("Login successful")
+              sessionStorage.setItem("user",JSON.stringify(res.data))//缓存用户信息
               this.$router.push("/welcome")
             } else {
               ElMessage.error(res.msg)
@@ -115,22 +108,32 @@ export default {
   position: fixed;
   width: 100%;
   height: 100vh;
-  background: url('../img/bg2.svg');
-  background-size: contain;
+  background: url('../img/bg3.jpg');
+  background-size: cover;
   overflow: hidden;
 }
 
 .login-page {
-  border-radius: 5px;
-  margin: 300px auto;
+  position: relative;
+  border-radius: 15px;
   width: 420px;
-  padding: 35px 35px 15px;
-  background: #fff;
+  padding: 0 50px;
   border: 1px solid #eaeaea;
-  box-shadow: 0 0 25px #cac6c6;
+  background-color: #fff;
+  box-shadow: 0 5px 25px rgba(0,0,0,0.25); 
+  left: 50%;
+  top: 50%;
+  transform: translate(-50%, -50%);
 }
+.title {
+            font-size: 35px;
+            font-weight: bold;
+            text-align: center;
+            line-height: 100px;
+        }
 
-/deep/ .el-input__inner {
-  height: 40px;
+
+::v-deep .el-input__inner{
+  height: 50px;
 }
 </style>
