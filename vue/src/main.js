632e64246f3939c495048a77d984ef0363aabf49
huo65 <1599272181@qq.com>
2024-04-22 16:42:49 +0800

    fix:配置语言包

--- vue/src/main.js
+++ vue/src/main.js
@@ -5,6 +5,7 @@ import store from './store'
 import '@/assets/css/global.css'
 
 import zhCn from 'element-plus/es/locale/lang/zh-cn'
+import enLocale  from 'element-plus/lib/locale/lang/en'; // 更改为英文语言包
 import ElementPlus from 'element-plus'
 import 'element-plus/dist/index.css'
 import '@/assets/icon/iconfont.js' // 图标
@@ -14,4 +15,4 @@ import * as ElIconModules from '@element-plus/icons'
 for(let iconName in ElIconModules){
     app.component(iconName,ElIconModules[iconName])
 }
-app.use(store).use(router).use(ElementPlus,{locale: zhCn,size:"small"}).mount('#app')
+app.use(store).use(router).use(ElementPlus,{locale: enLocale,size:"small"}).mount('#app')
