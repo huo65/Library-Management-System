3f184c831ce3d1e33742b779ea506cdb140ce123
huo65 <1599272181@qq.com>
2024-05-12 15:34:41 +0800

    对接支付宝

---
+++ SpringBoot/src/test/java/com/example/demo/AliPayTest.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.test.context.SpringBootTest;
+
+/**
+ * @author huo
+ * @date 2024/05/12 13:28
+ **/
+@SpringBootTest
+public class AliPayTest {
+//iflhrh3053@sandbox.com 111111 111111
+//http://localhost:8090/alipay/pay?subject=%E4%B8%9C%E5%8C%97%E8%8F%9C&traceNo=100001&totalAmount=100
+}
