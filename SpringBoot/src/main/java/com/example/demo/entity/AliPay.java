2146618f24e3861982090bd559be40c8352338a4
huo65 <1599272181@qq.com>
2024-05-12 15:34:41 +0800

    对接支付宝

---
+++ SpringBoot/src/main/java/com/example/demo/entity/AliPay.java
@@ -0,0 +1,15 @@
+package com.example.demo.entity;
+
+import lombok.Data;
+
+@Data
+public class AliPay {
+   //订单编号
+   private String traceNo;
+   //商品金额
+   private double totalAmount;
+   //商品名称
+   private String subject;
+   //订单追踪号，商户自己生成，可已不使用
+   private String alipayTraceNo;
+}
