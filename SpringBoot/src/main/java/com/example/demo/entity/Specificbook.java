7c4f5ab7fbad96a6e5ea779b7f2a379017c9383c
huo65 <1599272181@qq.com>
2024-06-01 10:20:52 +0800

    对象更新

---
+++ SpringBoot/src/main/java/com/example/demo/entity/Specificbook.java
@@ -0,0 +1,165 @@
+package com.example.demo.entity;
+
+import com.baomidou.mybatisplus.annotation.IdType;
+import com.baomidou.mybatisplus.annotation.TableField;
+import com.baomidou.mybatisplus.annotation.TableId;
+import com.baomidou.mybatisplus.annotation.TableName;
+import com.fasterxml.jackson.annotation.JsonFormat;
+import lombok.Data;
+
+import java.io.Serializable;
+import java.util.Date;
+
+
+/**
+ * 具体书籍表
+ * @TableName specificbook
+ */
+@TableName(value ="specificbook")
+public class Specificbook implements Serializable {
+    /**
+     * ID
+     */
+    @TableId(type = IdType.AUTO)
+    private Integer id;
+
+    /**
+     * 图书编号
+     */
+    private String isbn;
+
+    /**
+     * 名称
+     */
+    private String name;
+
+    /**
+     * 0：未归还 1：已归还
+     */
+    private String status;
+
+    /**
+     * 存放位置
+     */
+    private String location;
+
+    @TableField(exist = false)
+    private static final long serialVersionUID = 1L;
+
+    /**
+     * ID
+     */
+    public Integer getId() {
+        return id;
+    }
+
+    /**
+     * ID
+     */
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    /**
+     * 图书编号
+     */
+    public String getIsbn() {
+        return isbn;
+    }
+
+    /**
+     * 图书编号
+     */
+    public void setIsbn(String isbn) {
+        this.isbn = isbn;
+    }
+
+    /**
+     * 名称
+     */
+    public String getName() {
+        return name;
+    }
+
+    /**
+     * 名称
+     */
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    /**
+     * 0：未归还 1：已归还
+     */
+    public String getStatus() {
+        return status;
+    }
+
+    /**
+     * 0：未归还 1：已归还
+     */
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    /**
+     * 存放位置
+     */
+    public String getLocation() {
+        return location;
+    }
+
+    /**
+     * 存放位置
+     */
+    public void setLocation(String location) {
+        this.location = location;
+    }
+
+    @Override
+    public boolean equals(Object that) {
+        if (this == that) {
+            return true;
+        }
+        if (that == null) {
+            return false;
+        }
+        if (getClass() != that.getClass()) {
+            return false;
+        }
+         Specificbook other = ( Specificbook) that;
+        return (this.getId() == null ? other.getId() == null : this.getId().equals(other.getId()))
+                && (this.getIsbn() == null ? other.getIsbn() == null : this.getIsbn().equals(other.getIsbn()))
+                && (this.getName() == null ? other.getName() == null : this.getName().equals(other.getName()))
+                && (this.getStatus() == null ? other.getStatus() == null : this.getStatus().equals(other.getStatus()))
+                && (this.getLocation() == null ? other.getLocation() == null : this.getLocation().equals(other.getLocation()));
+    }
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((getId() == null) ? 0 : getId().hashCode());
+        result = prime * result + ((getIsbn() == null) ? 0 : getIsbn().hashCode());
+        result = prime * result + ((getName() == null) ? 0 : getName().hashCode());
+        result = prime * result + ((getStatus() == null) ? 0 : getStatus().hashCode());
+        result = prime * result + ((getLocation() == null) ? 0 : getLocation().hashCode());
+        return result;
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        sb.append(getClass().getSimpleName());
+        sb.append(" [");
+        sb.append("Hash = ").append(hashCode());
+        sb.append(", id=").append(id);
+        sb.append(", isbn=").append(isbn);
+        sb.append(", name=").append(name);
+        sb.append(", status=").append(status);
+        sb.append(", location=").append(location);
+        sb.append(", serialVersionUID=").append(serialVersionUID);
+        sb.append("]");
+        return sb.toString();
+    }
+}
---
+++ SpringBoot/src/main/java/com/example/demo/service/BookService.java
@@ -0,0 +1,14 @@
+package com.example.demo.service;
+
+
+import com.baomidou.mybatisplus.extension.service.IService;
+import com.example.demo.entity.Book;
+
+/**
+* @author 86132
+* @description 针对表【book】的数据库操作Service
+* @createDate 2024-06-01 09:53:24
+*/
+public interface BookService extends IService<Book> {
+
+}
